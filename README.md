3c0a1addcc45df672d9ddf88ca2eedb34dc54bc1
Marcelo Cerri <mhcerri@gmail.com>
2016-03-02 17:22:46 -0300

    Add travis support.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.org/PromonLogicalis/snmp.svg?branch=master)](https://travis-ci.org/PromonLogicalis/snmp)
 # snmp
 --
     import "github.com/PromonLogicalis/snmp"
--- gen_readme.sh
+++ gen_readme.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-godocdown ./ > ./README.md
+#!/bin/bash
+
+# Add Travis badge:
+cat > ./README.md << 'EOF'
+[![Build Status](https://travis-ci.org/PromonLogicalis/snmp.svg?branch=master)](https://travis-ci.org/PromonLogicalis/snmp)
+EOF
+
+# Add Go doc
+godocdown ./ >> ./README.md
